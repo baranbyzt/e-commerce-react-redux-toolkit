bf2520e8f271ec959a1c65dd32159da49c508dac
byztmain <byztmain2121@gmail.com>
2022-09-09 15:04:05 +0300

    git inits

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // navigation
-import Navigation from './Navigation';
+import Navigation from "./Navigation";
 // style
-import './stylings/General.css'
+import "./stylings/General.css";
 // our store
-import { Provider} from 'react-redux'
-import store from './store/Store'
+import { Provider } from "react-redux";
+import store from "./store/Store";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<Provider store={store}>
-<Navigation />
-</Provider>
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   </React.StrictMode>
 );
-
-/*
-
-
-
-
-
-
-<Provider store={store}>
-<Navigation />
-</Provider>
-*/
