7716ca0600d5b92a0d2a2dc47540deddca0ede9f
byztmain <byztmain2121@gmail.com>
2022-09-16 12:26:03 +0300

    just test

--- src/pages/BasketPage.js
+++ src/pages/BasketPage.js
@@ -1,58 +1,53 @@
-
-
-import React,{useState} from 'react'
-// style 
-import style from '../stylings/BasketPage.module.css'
+import React, { useState } from "react";
+// style
+import style from "../stylings/BasketPage.module.css";
 // component
-import BasketCards from '../component/BasketCards';
+import BasketCards from "../component/BasketCards";
 
 // store
-import { useSelector,useDispatch } from 'react-redux';
-import {selectmoney,selectTodos} from '../store/CardSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { selectmoney, selectTodos } from "../store/CardSlice";
 
 const BasketPage = () => {
+  // console.log("test")
+  const todos = useSelector(selectTodos);
+  const todosmoney = useSelector(selectmoney);
 
-        const todos = useSelector(selectTodos);
-        const todosmoney = useSelector(selectmoney);
+  let kdv = todosmoney.total_spend / 18;
 
-let kdv = todosmoney.total_spend/18
+  let cargo = (totalmonney) => {
+    if (totalmonney > 100) {
+      return totalmonney + kdv;
+    } else {
+      return totalmonney + kdv + 30;
+    }
+  };
 
-let cargo= (totalmonney) => {
-if(totalmonney > 100)
-{
-return totalmonney+kdv
-}else{
-       return totalmonney+kdv+30
-}
-}
+  let cargo2 = (totalmonney) => {
+    if (totalmonney > 100) {
+      return "FREE";
+    } else {
+      return 30;
+    }
+  };
 
-let cargo2 = (totalmonney) => {
-        if(totalmonney > 100)
-        {
-        return 'FREE'
-        }else{
-               return 30
-        }
-        }
-
-    return ( 
-        <div className={style.wrapper}>
-<div className={style.itemssection}>
-{todos.map((todo, key) => (
-        <BasketCards key={key} data={todo} />
-      ))}
-</div>
-<div className={style.bill}>
-<div>
-<p>sepet toplam:        {todosmoney.total_spend} ₺</p>
-<p>%18-kdv:         {kdv.toFixed(2)} ₺</p>
-<p>kargo masrafı: {cargo2(todosmoney.total_spend)} </p>
-<p>TOPLAM:            {cargo(todosmoney.total_spend).toFixed(2)} ₺</p>
-</div>
-</div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.itemssection}>
+        {todos.map((todo, key) => (
+          <BasketCards key={key} data={todo} />
+        ))}
+      </div>
+      <div className={style.bill}>
+        <div>
+          <p>sepet toplam: {todosmoney.total_spend} ₺</p>
+          <p>%18-kdv: {kdv.toFixed(2)} ₺</p>
+          <p>kargo masrafı: {cargo2(todosmoney.total_spend)} </p>
+          <p>TOPLAM: {cargo(todosmoney.total_spend).toFixed(2)} ₺</p>
         </div>
-     );
-}
- 
-export default BasketPage;
+      </div>
+    </div>
+  );
+};
 
+export default BasketPage;
